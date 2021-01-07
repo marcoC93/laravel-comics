9f0f8317497855ff45b4b839a333701cc8c6c517
Marco <marco.chianese93@gmail.com>
2021-01-07 20:02:01 +0100

    creazione menu header (v1.0)

---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,80 @@
+<header>
+    <div class="header-top">
+        <div class="container">
+            <nav>
+                <ul>
+                    <li> dc power visa</li>
+                    <li> additional dc sites</li>
+                </ul>
+
+            </nav>
+        </div>
+    </div>
+    <div class="header-bottom">
+        <div class="container">
+            <div class="flex-container">
+                <div class="logo">
+                    <img src="{{asset('img/dc-logo.png')}}" alt="">
+                </div>
+                <div class="main-menu">
+                    <ul>
+                        <li>
+                            <a href="#">
+                                characters
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                comics
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                movies
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                tv
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                games
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                collectibles
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                videos
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                fans
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                news
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                shop
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="search-bar">
+
+                    <input type="text" name="" value="">
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
