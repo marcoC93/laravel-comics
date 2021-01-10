725ef8526ffb002d5527785c42347623c87fcd10
Marco <marco.chianese93@gmail.com>
2021-01-11 00:16:28 +0100

    stampa dinamica autori (v1.0)

--- resources/views/single.blade.php
+++ resources/views/single.blade.php
@@ -1,10 +1,22 @@
 @extends('layout.layout')
 
 @section('content')
-    <div class="row">
-        <div class="col-md-12">
-            <h1> {{ $comic['title']}}</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1> {{ $comic['title']}}</h1>
+                <img src="{{$comic['thumb']}}" alt="">
+                <p> {{$comic['description']}}</p>
+                <ul>
+                    @foreach ($comic['artists'] as $artist)
+                    <li>
+                        <a href="#">
+                            {{$artist}}
+                        </a>     
+                    </li>
+                    @endforeach
+                </ul>
+            </div>
         </div>
     </div>
-
 @endsection
